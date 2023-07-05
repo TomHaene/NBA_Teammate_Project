b1cac2396ca14803a5d7e053bd3217f266cf83d4
TomHaene <tom@haene.org>
2023-07-05 14:31:57 +0400

    Nevermind, had to turn off DEBUG mode

--- NBAProject/settings.py
+++ NBAProject/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h9@=h1s6&-@80sak-2i0&mfmmz&qg6izq0^aovhjg&e7d5_fhl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    'tomhaene.pythonanywhere.com'
+
 ]
 
 
