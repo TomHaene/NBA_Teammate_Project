1b430bf357120a2232b3435cb478a0bfc51dcf1f
TomHaene <tom@haene.org>
2023-07-06 14:34:01 +0400

    Newest version

--- NBA_Logic/Tom.py
+++ NBA_Logic/Tom.py
@@ -4,8 +4,8 @@ from nba_api.stats.static import players, teams
 import pandas as pd
 from nba_api.stats.endpoints import playergamelog, teamgamelog, leaguegamefinder, commonplayerinfo, commonallplayers
 from nba_api.stats.library.parameters import SeasonAll
-
 from nba_api.stats.endpoints import playercareerstats
+
 
 all_players_dict = players.get_players()
 nba_players_df = pd.DataFrame(all_players_dict)
@@ -75,10 +75,21 @@ def setup(name):
 careerStatsRegularDF= 0 
 careerStatsPlayoffsDF= 0 
 
+custom_headers = {
+    'Host': 'stats.nba.com',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+}
+
 
 def getCareerStatsRegularSeason(playerID):
     global careerStatsRegularDF
-    careerStats = playercareerstats.PlayerCareerStats(player_id = playerID)
+    careerStats = playercareerstats.PlayerCareerStats(player_id = playerID,proxy='180.180.171.121:8080',headers=custom_headers,timeout=100)
     career_stats_data = careerStats.get_data_frames()[0]
     regularSeasonTotalsDataSet = careerStats.career_totals_regular_season
     careerStatsDF = regularSeasonTotalsDataSet.get_data_frame()
@@ -90,7 +101,7 @@ def getCareerStatsRegularSeason(playerID):
 
 def getCareerStatsPlayoffs(playerID):
     global careerStatsPlayoffsDF
-    careerStats = playercareerstats.PlayerCareerStats(player_id = playerID)
+    careerStats = playercareerstats.PlayerCareerStats(player_id = playerID,proxy='180.180.171.121:8080',headers=custom_headers, timeout=100)
     career_stats_data = careerStats.get_data_frames()[0]
     regularSeasonTotalsDataSet = careerStats.career_totals_post_season
     careerStatsDF = regularSeasonTotalsDataSet.get_data_frame()
